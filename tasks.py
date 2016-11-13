7b9195c483680e32a1e30aff929e68f4ac851579
michael <michaelhuynh4000@gmail.com>
2016-11-13 17:36:08 +1100

    First commit - Create Add fuctionality, No database yet

---
+++ tasks.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+from datetime import date 
+import argparse
+
+#add command arguments 
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", "--add", action="store_true", help="add a new task")
+args = parser.parse_args()
+
+class Task:
+
+	id = 0
+	done = False
+	title = "untitled"
+	due = date.today()
+
+	def __init__(self, id):
+		self.id = id
+			
+	def print(self):
+		print('{0:<10}{1:<10}{2:<10}{3:<10}'.format(self.id, self.done, self.due.strftime("%d/%m/%y"), self.title))
+
+class Tasks:
+	
+	tasks = []
+
+	def __init__(self):
+		id = len(self.tasks)
+		self.tasks.append(Task(id)) 	
+
+	def add(self):
+		id = len(self.tasks)
+		self.tasks.append(Task(id))	
+		
+	def print(self):
+		for t in self.tasks:
+			t.print()
+
+tasks = Tasks()
+
+#add new task
+if args.add:
+	tasks.add()
+
+#print out all tasks
+tasks.print()
+
+	
